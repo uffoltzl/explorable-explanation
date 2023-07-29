98262ff19d473f5ccb56a4469135a97d347bddd6
uffoltzl <l.ufz@hotmail.fr>
2023-07-29 22:18:36 +0200

    https://www.obytes.com/blog/create-drawing-app

--- src/DetectLetters/DetectLetters.tsx
+++ src/DetectLetters/DetectLetters.tsx
@@ -1,10 +1,74 @@
-import { FunctionComponent } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FunctionComponent, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  GestureResponderEvent,
+} from "react-native";
+import { Svg, Path } from "react-native-svg";
+
+const { height, width } = Dimensions.get("window");
 
 export const DetectLetters: FunctionComponent = () => {
+  const [paths, setPaths] = useState<string[][]>([]);
+  const [currentPath, setCurrentPath] = useState<string[]>([]);
+
+  const onTouchMove = (event: GestureResponderEvent) => {
+    const newPath = [...currentPath];
+
+    //get current user touches position
+    const locationX = event.nativeEvent.locationX;
+    const locationY = event.nativeEvent.locationY;
+
+    // create new point
+    const newPoint = `${newPath.length === 0 ? "M" : ""}${locationX.toFixed(
+      0
+    )},${locationY.toFixed(0)} `;
+
+    // add the point to older points
+    newPath.push(newPoint);
+    setCurrentPath(newPath);
+  };
+  const onTouchEnd = () => {
+    const currentPaths = [...paths];
+    const newPath = [...currentPath];
+
+    //push new path with old path and clean current path state
+    currentPaths.push(newPath);
+    setPaths(currentPaths);
+    setCurrentPath([]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Detect letters</Text>
+      <View
+        style={styles.svgContainer}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
+        <Svg height={height * 0.7} width={width}>
+          <Path
+            d={currentPath.join("")}
+            stroke={"green"}
+            fill={"transparent"}
+            strokeWidth={2}
+            strokeLinejoin={"round"}
+            strokeLinecap={"round"}
+          />
+
+          {paths.length > 0 &&
+            paths.map((path, index) => (
+              <Path
+                key={`path-${index}`}
+                d={path.join("")}
+                stroke={"red"}
+                fill={"transparent"}
+                strokeWidth={2}
+                strokeLinejoin={"round"}
+                strokeLinecap={"round"}
+              />
+            ))}
+        </Svg>
+      </View>
     </View>
   );
 };
@@ -15,5 +79,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  svgContainer: {
+    height: height * 0.7,
+    width,
+    borderColor: "black",
+    backgroundColor: "white",
+    borderWidth: 1,
   },
 });
